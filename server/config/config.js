37151e29e94fe81fa37d1f4412cc7941e0e03351
Dan Le <dhl1337@gmail.com>
2016-11-28 12:43:34 -0600

    first commit

---
+++ server/config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+db: 'mongodb://localhost:27017/waitr',
+  //db: 'mongodb://waitr:dm82016!@ds015928.mongolab.com:15928/waitr',
+  secretKey: 'SUPER_SECRET_KEY',
+  ACCOUNT_SID: 'AC2f284f180794050d569493bc7aefaf52',
+  AUTH_TOKEN: '400c9a10ef1eb505e1803319614bf0de'
+};
