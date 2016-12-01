cb6e6cdc36d4e3f3b5f70a2803db271dabcb0c67
Dan Le <dhl1337@gmail.com>
2016-12-01 12:39:11 -0600

    add wait list controller to es6

---
+++ www/app/customer/waitlist/customer-waitlist.controller.js
@@ -0,0 +1,61 @@
+(function () {
+    angular
+        .module('waitrApp')
+        .controller('CustomerWaitlistController', ['userService', 'restaurantService', '$scope', 'waitlistService', '$ionicPopup', '$state', '$ionicHistory', CustomerWaitlistController]);
+
+    function CustomerWaitlistController(userService, restaurantService, $scope, waitlistService, $ionicPopup, $state, $ionicHistory) {
+
+        const vm = this;
+        const socket = io.connect('http://localhost:3000');
+
+        vm.currentUser = $scope.ccc.currentUser;
+
+        socket.on('newPersonAdded', (data) => {
+            vm.currentUser.inWaitList.list.push(data);
+            $scope.$apply();
+        });
+
+        socket.on('deletedPerson', (data) => {
+            if (vm.currentUser.inWaitList) {
+                vm.currentUser.inWaitList.list.splice(data.pos, 1);
+                $scope.$apply();
+            }
+        });
+
+        userService.currentUser(vm.currentUser._id).then((user) => {
+            vm.currentUser = user[0];
+
+            restaurantService.getCurrentRestaurant(vm.currentUser.inWaitList.restaurant_id).then((data) => vm.restaurant = data[0]);
+        });
+
+        vm.removeFromWaitlist = function () {
+            let list = vm.currentUser.inWaitList.list;
+            for (let i = 0; i < list.length; i++) {
+                if (list[i].user_id == vm.currentUser._id) {
+                    waitlistService.removeFromWaitlist(list[i]._id, vm.currentUser.inWaitList._id).then((res) => {
+                        $scope.ccc.currentUser.inWaitList = undefined;
+                        socket.emit('deletePerson', res);
+                        $ionicHistory.nextViewOptions({
+                            disableBack: true
+                        });
+                        $state.go("customer.home");
+                    });
+                }
+            }
+        };
+
+        vm.showRemovePopup = function () {
+            let confirmPopup = $ionicPopup.confirm({
+                title: "Remove from waitlist",
+                template: "WARNING: this will remove you from the list"
+            });
+
+            confirmPopup.then(function (res) {
+                if (res) {
+                    vm.removeFromWaitlist();
+                }
+            })
+        }
+    }
+
+})();
