405ed472608f30a93ba235fe695f06fdb4357eba
Dan Le <dhl1337@gmail.com>
2016-12-04 22:05:27 -0600

    change user service to es6

--- www/app/services/userService.js
+++ www/app/services/userService.js
@@ -1,29 +1,15 @@
 (function () {
-  angular
-    .module('waitrApp')
-    .service('userService', ['$http', 'SERVER_URL', userService]);
+    'use strict';
+    angular
+        .module('waitrApp')
+        .service('userService', ['$http', 'SERVER_URL', userService]);
 
-  function userService($http, SERVER_URL) {
+    function userService($http, SERVER_URL) {
 
-    this.currentUser = function (id) {
-      return $http({
-        method: 'GET',
-        url: SERVER_URL + '/api/user/' + id
-      }).then(function (response) {
-        return response.data;
-      });
-    };
+        this.currentUser = (id) => $http.get(`${SERVER_URL}/api/user/${id}`).then(response => response.data);
 
-    this.updateUser = function (id, obj) {
-      return $http({
-        method: 'PUT',
-        url: SERVER_URL + '/api/user/' + id,
-        data: obj
-      }).then(function (response) {
-        return response.data;
-      });
-    };
+        this.updateUser = (id, obj) => $http.put(`${SERVER_URL}/api/user/${id}`).then(response => response.data);
 
-  }
+    }
 
 })();
