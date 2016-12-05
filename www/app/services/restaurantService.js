393b1f61af497b616591275f78383902f4f12cff
Dan Le <dhl1337@gmail.com>
2016-12-04 21:51:01 -0600

    change restaurant service to es6

--- www/app/services/restaurantService.js
+++ www/app/services/restaurantService.js
@@ -1,55 +1,24 @@
 (function () {
-  angular
-    .module('waitrApp')
-    .service('restaurantService', ['$http', 'SERVER_URL', restaurantService]);
+    angular
+        .module('waitrApp')
+        .service('restaurantService', ['$http', 'SERVER_URL', restaurantService]);
 
-  function restaurantService($http, SERVER_URL) {
+    function restaurantService($http, SERVER_URL) {
 
-    this.notification = function (obj) {
-      return $http.put(SERVER_URL + '/api/twilio', obj).then(function(response) {
-        return response.data;
-      })
-    };
+        this.notification = (obj) => $http.put(`${SERVER_URL}/api/twilio`, obj).then(response => response.data);
 
-    this.getRestaurants = function () {
-      return $http.get(SERVER_URL + '/api/restaurant').then(function(response) {
-        return response.data;
-      });
-    };
+        this.getRestaurants = () => $http.get(`${SERVER_URL}/api/restaurant`).then(response => response.data);
 
-    this.getCurrentRestaurant = function (id) {
-      return $http.get(SERVER_URL + '/api/restaurant/' + id).then(function(response){
-        return response.data;
-      });
-    };
+        this.getCurrentRestaurant = (id) => $http.get(`${SERVER_URL}/api/restaurant/${id}`).then(response => response.data);
 
-    this.updateRestaurant = function (id, obj) {
-      return $http.put(SERVER_URL + '/api/restaurant/' + id, obj).then(function(response) {
-        return response.data;
-      });
-    };
+        this.updateRestaurant = (id, obj) => $http.put(`${SERVER_URL}/api/restaurant/${id}`, obj).then(response => response.data);
 
-    this.updateRestaurantMenu = function (id, menuObj) {
-      return $http.put(SERVER_URL + '/api/restaurant/menu/add/' + id, menuObj)
-        .then(function (response) {
-          return response.data;
-        });
-    };
-    
-    this.deleteRestaurantMenuItem = function (id, item) {
-      return $http.put(SERVER_URL + '/api/restaurant/menu/remove/' + id, item)
-        .then(function(response) {
-          return response.data;
-        })
-    };
-     
-    this.getWaitlist = function (waitListId) {
-      return $http.get(SERVER_URL + '/api/waitlist/?restaurant_id=' + waitListId).then(function (response) {
-        return response.data;
-      });
-    };
+        this.updateRestaurantMenu = (id, menuObj) => $http.put(`${SERVER_URL}/api/restaurant/menu/add/${id}`, menuObj).then(response => response.data);
 
+        this.deleteRestaurantMenuItem = (id, item) => $http.put(`${SERVER_URL}/api/restaurant/menu/remove/${id}`, item).then(response => response.data);
 
-  }
+        this.getWaitlist = (waitListId) => $http.get(`${SERVER_URL}/api/waitlist/?restaurant_id=${waitListId}`).then(response => response.data);
+
+    }
 
 })();
