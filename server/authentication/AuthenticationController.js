738c8a382f2403f9d0a36bb1256afddc7285fd15
Dan Le <dhl1337@gmail.com>
2016-12-05 21:21:59 -0600

    change spaces for auth and config module

--- server/authentication/AuthenticationController.js
+++ server/authentication/AuthenticationController.js
@@ -5,56 +5,56 @@ const config = require('../config/config');
 const Restaurant = require('../restaurant/RestaurantModel');
 
 module.exports = {
-  register(req, res) {
-    const regCust = (req, res) => {
-      if (req.body.restaurant_id) {
-        req.body.role = 'restaurant';
-      }
-      const newUser = new User(req.body);
-      newUser.password = newUser.generateHash(req.body.password);
-      newUser.save((err, user) => {
-        if (err)
-          res.status(500).send(err);
-        else {
-          let payload = user.toObject();
-          let token = jwt.sign(payload, config.secretKey); // { expiresIn: 600 } expires in 10 minutes
-          res.status(200).json({token: token});
-        }
-      });
-    };
-
-    if (req.body.restaurantName) {
-      let newRestaurant = new Restaurant({restaurantName: req.body.restaurantName});
-      newRestaurant.save((err, restaurant) => {
-        if (err)
-          res.status(500).send(err);
-        else {
-          let newWaitlist = new Waitlist({restaurant_id: restaurant._id});
-          newWaitlist.save((err, waitlist) => {
-            Restaurant.findByIdAndUpdate(restaurant._id, {$set: {waitlist_id: waitlist._id}}, (err, restaurant) => {
-              err ? res.status(500).send(err) : req.body.restaurant_id = restaurant._id;
-              regCust(req, res);
+    register(req, res) {
+        const regCust = (req, res) => {
+            if (req.body.restaurant_id) {
+                req.body.role = 'restaurant';
+            }
+            const newUser = new User(req.body);
+            newUser.password = newUser.generateHash(req.body.password);
+            newUser.save((err, user) => {
+                if (err)
+                    res.status(500).send(err);
+                else {
+                    let payload = user.toObject();
+                    let token = jwt.sign(payload, config.secretKey); // { expiresIn: 600 } expires in 10 minutes
+                    res.status(200).json({token: token});
+                }
             });
-          });
-        }
-      });
-    } else {
-      regCust(req, res);
-    }
-  },
+        };
 
-  login(req, res) {
-    User.findOne({email: req.body.email}, (err, user) => {
-      if (user) {
-        if (!user.validPassword(req.body.password))
-          res.status(401).send('Wrong password. Try again');
-        else {
-          let payload = user.toObject();
-          let token = jwt.sign(payload, config.secretKey, {expiresIn: 3600});
-          res.status(200).json({token: token});
+        if (req.body.restaurantName) {
+            let newRestaurant = new Restaurant({restaurantName: req.body.restaurantName});
+            newRestaurant.save((err, restaurant) => {
+                if (err)
+                    res.status(500).send(err);
+                else {
+                    let newWaitlist = new Waitlist({restaurant_id: restaurant._id});
+                    newWaitlist.save((err, waitlist) => {
+                        Restaurant.findByIdAndUpdate(restaurant._id, {$set: {waitlist_id: waitlist._id}}, (err, restaurant) => {
+                            err ? res.status(500).send(err) : req.body.restaurant_id = restaurant._id;
+                            regCust(req, res);
+                        });
+                    });
+                }
+            });
+        } else {
+            regCust(req, res);
         }
-      }
-      else res.status(401).send('User not found');
-    });
-  }
+    },
+
+    login(req, res) {
+        User.findOne({email: req.body.email}, (err, user) => {
+            if (user) {
+                if (!user.validPassword(req.body.password))
+                    res.status(401).send('Wrong password. Try again');
+                else {
+                    let payload = user.toObject();
+                    let token = jwt.sign(payload, config.secretKey, {expiresIn: 3600});
+                    res.status(200).json({token: token});
+                }
+            }
+            else res.status(401).send('User not found');
+        });
+    }
 };
--- server/authentication/AuthenticationRoute.js
+++ server/authentication/AuthenticationRoute.js
@@ -1,6 +1,6 @@
 const AuthenticationController = require('./AuthenticationController');
 
 module.exports = app => {
-  app.post('/register', AuthenticationController.register);
-  app.post('/login', AuthenticationController.login);
+    app.post('/register', AuthenticationController.register);
+    app.post('/login', AuthenticationController.login);
 };
