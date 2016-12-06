dd755abf217d2df7d7530296e90395e885d7dad8
Dan Le <dhl1337@gmail.com>
2016-12-05 21:21:18 -0600

    change spacing in restaurant module

--- server/restaurant/RestaurantRoute.js
+++ server/restaurant/RestaurantRoute.js
@@ -1,14 +1,14 @@
 var RestaurantController = require('./RestaurantController');
 
 module.exports = app => {
-  app.get('/api/restaurant', RestaurantController.read);
-  app.get('/api/restaurant/:id', RestaurantController.currentRestId);
+    app.get('/api/restaurant', RestaurantController.read);
+    app.get('/api/restaurant/:id', RestaurantController.currentRestId);
 
-  app.post('/api/restaurant', RestaurantController.create);
+    app.post('/api/restaurant', RestaurantController.create);
 
-  app.put('/api/restaurant/:id', RestaurantController.update);
-  app.put('/api/restaurant/menu/add/:id', RestaurantController.addItemToMenu);
-  app.put('/api/restaurant/menu/remove/:id', RestaurantController.deleteItemToMenu);
+    app.put('/api/restaurant/:id', RestaurantController.update);
+    app.put('/api/restaurant/menu/add/:id', RestaurantController.addItemToMenu);
+    app.put('/api/restaurant/menu/remove/:id', RestaurantController.deleteItemToMenu);
 
-  app.delete('/api/restaurant/:id', RestaurantController.delete);
+    app.delete('/api/restaurant/:id', RestaurantController.delete);
 };
